46bfee6d485c3f4e2e12c733a70a4a1fa95393e0
percevalkm <perceval@sun.ac.za>
2019-04-23 11:44:28 +0200

    trap filters

--- TRAP/admin.py
+++ TRAP/admin.py
@@ -8,8 +8,8 @@ from TRAP.resources import TsetseResource
 
 class TsetseAdmin(ImportExportModelAdmin):
 	list_display = ('caseno2','vpn','volume','page','number_on_page','capture_day_of_month','capture_month_year','capture_year','experimental_month','capture_days_since_01011960','disector_code','microscope','microscope','capture_sites')
-	#list_filter = ['capture_year']
-	#list_per_page =50
+	list_filter = ['capture_year','capture_month_year','capture_day_of_month']
+	list_per_page =100
 	
 
 	
