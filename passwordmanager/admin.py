9f449237064aab71277a4dec539c546b12037e7d
percevalkm <perceval@sun.ac.za>
2019-04-25 11:58:17 +0200

    importexport on passwordmanager

--- passwordmanager/admin.py
+++ passwordmanager/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Account
-
-# Register your models here.
-class AccountAdmin(admin.ModelAdmin):
+from import_export.admin import ImportExportModelAdmin
+from passwordmanager.resources import AccountResource
+# Register your models here.(admin.ModelAdmin):
+class AccountAdmin(ImportExportModelAdmin):
 	list_display = ('id','account_name','username','password','created_at','updated_at')
 	ordering =	['id']
 	search_fields =('account_name','username')
