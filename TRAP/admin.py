b69aafe7646e8f3fd1fed3ec5a55baf5c36e9da1
percevalm <perceval@sun.ac.za>
2019-04-28 00:00:27 +0200

    added fields to Trap model

--- TRAP/admin.py
+++ TRAP/admin.py
@@ -7,13 +7,10 @@ from TRAP.resources import TsetseResource
 
 
 class TsetseAdmin(ImportExportModelAdmin):
-	list_display = [f.name for f in Tsetse._meta.fields]
+	list_display = [f.name for f in Tsetse._meta.fields]# this code loops all columns and displays all columns in django admin
 	#list_display = ('caseno2','vpn','volume','page','number_on_page','capture_day_of_month','capture_month_year
 	list_filter = ['capture_year','capture_month_year','capture_day_of_month']
 	ordering = ['page']
-	page_hierachy = 'page'
-
-	
 
 	
 	resource_class = TsetseResource #Configure the resource class
