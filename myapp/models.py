e6aafc29cdf376b3eeebebd31a28e94f5c1392a2
percevalkm <perceval@sun.ac.za>
2019-05-22 10:58:07 +0200

    verbose name on field names

--- myapp/models.py
+++ myapp/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 # Create your models here. # these will have column names and column properties
 class Intbursary(models.Model):
-	
+
 	surname = models.CharField(max_length=200)
 	first_name = models.CharField(max_length=200)
 	id_number = models.CharField(max_length=200)
@@ -48,7 +48,7 @@ class Intbursary(models.Model):
 
 	employed = models.CharField(max_length = 10, choices = EMPLOYED_CHOICES)
 	employed_at = models.CharField(max_length=200)
-	
+
 	FullTime = 'FullTime'
 	PartTime = 'PartTime'
 
@@ -58,7 +58,7 @@ class Intbursary(models.Model):
 		)
 
 	full_part_time = models.CharField(max_length = 10, choices = STUDY_CHOICES)
-	
+
 	Yes = 'Yes'
 	Yes = 'Yes'
 
@@ -68,7 +68,7 @@ class Intbursary(models.Model):
 		)
 
 	employed_study = models.CharField(max_length = 3, choices = EMPLOYED_STUDY_CHOICES)
-	
+
 	employed_study_details=models.TextField(max_length=200)
 	Msc = 'Msc'
 	PhD = 'PhD'
@@ -78,29 +78,29 @@ class Intbursary(models.Model):
 		)
 
 	proposed_degree = models.CharField(max_length = 3, choices = DEGREE_CHOICES)
-	
+
 	other_funding = models.TextField(max_length=200)
 	referee_details = models.TextField(max_length =200)
-	degree_1 = models.CharField(max_length=200, blank =True)
-	f_o_study_1 = models.CharField(max_length=200, blank = True)
-	major_sub_1 = models.CharField(max_length=200, blank = True)
-	institution_1 = models.CharField(max_length=200, blank = True)
-	year_obtained_1 = models.CharField(max_length=10, blank =True)
-	degree_2 = models.CharField(max_length=200, blank =True)
-	f_o_study_2 = models.CharField(max_length=200, blank = True)
-	major_sub_2 = models.CharField(max_length=200, blank = True)
-	institution_2 = models.CharField(max_length=200, blank = True)
-	year_obtained_2 = models.CharField(max_length=10, blank = True)
-	degree_3 = models.CharField(max_length=200, blank =True)
-	f_o_study_3 = models.CharField(max_length=200, blank = True)
-	major_sub_3 = models.CharField(max_length=200, blank = True)
-	institution_3 = models.CharField(max_length=200, blank = True)
-	year_obtained_3 = models.CharField(max_length=10, blank=True)
-	degree_4 = models.CharField(max_length=200, blank =True)
-	f_o_study_4 = models.CharField(max_length=200, blank = True)
-	major_sub_4 = models.CharField(max_length=200, blank = True)
-	institution_4 = models.CharField(max_length=200, blank = True)
-	year_obtained_4 = models.CharField(max_length=10, blank= True)
+	degree_1 = models.CharField(max_length=200, blank =True, verbose_name=('Degree 1'))
+	f_o_study_1 = models.CharField(max_length=200, blank = True,verbose_name=('Field of study 1'))
+	major_sub_1 = models.CharField(max_length=200, blank = True,verbose_name=('Major Subject 1'))
+	institution_1 = models.CharField(max_length=200, blank = True, verbose_name=('Institution 1'))
+	year_obtained_1 = models.CharField(max_length=10, blank =True,verbose_name=('Year obtained 1'))
+	degree_2 = models.CharField(max_length=200, blank =True, verbose_name=('Degree 2'))
+	f_o_study_2 = models.CharField(max_length=200, blank = True,verbose_name=('Field of study 2'))
+	major_sub_2 = models.CharField(max_length=200, blank = True, verbose_name=('Major subject 2'))
+	institution_2 = models.CharField(max_length=200, blank = True, verbose_name=('Institution 2'))
+	year_obtained_2 = models.CharField(max_length=10, blank = True,verbose_name=('Year obtained 2'))
+	degree_3 = models.CharField(max_length=200, blank =True, verbose_name=('Degree 3'))
+	f_o_study_3 = models.CharField(max_length=200, blank = True, verbose_name=('Field of study 3'))
+	major_sub_3 = models.CharField(max_length=200, blank = True, verbose_name=('Major subject 3'))
+	institution_3 = models.CharField(max_length=200, blank = True, verbose_name=('Institution 3'))
+	year_obtained_3 = models.CharField(max_length=10, blank=True,verbose_name=('Year obtained 3'))
+	degree_4 = models.CharField(max_length=200, blank =True,verbose_name=('Degree 4'))
+	f_o_study_4 = models.CharField(max_length=200, blank = True,verbose_name=('Field of study 4'))
+	major_sub_4 = models.CharField(max_length=200, blank = True,verbose_name=('Major subject 4'))
+	institution_4 = models.CharField(max_length=200, blank = True, verbose_name=('Institution 4'))
+	year_obtained_4 = models.CharField(max_length=10, blank= True, verbose_name=('Year obtained 4'))
 	"""database will store the directory of the file in the database instead of the file to avoid database
 	perfomance issues
 	"""
@@ -119,4 +119,3 @@ class Intbursary(models.Model):
 
 	def __str__(self):
 		return self.first_name + ' | '  + str (self.surname) + ' | '  + str (self.telephone_number)+ ' | '  + str (self.email) + ' | ' + str(self.id) + '|' + str(self.created_at) +'|' + str(self.proposed_degree) + '|'+ str(self.full_part_time)
-
