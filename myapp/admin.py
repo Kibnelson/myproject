16e31d9c687ace054068fb4d15fa58ea28e55187
percevalkm <perceval@sun.ac.za>
2019-07-03 09:57:26 +0200

    adding a horizontal line on top bar

--- myapp/admin.py
+++ myapp/admin.py
@@ -13,7 +13,7 @@ def export_to_csv (modeladmin, request, queryset):
 	response = HttpResponse(content_type='text/csv')
 	response [ 'Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
 	writer = csv.writer(response)
-	
+
 	fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
 	#write the header row
 	writer.writerow([field.verbose_name for field in fields])
@@ -49,13 +49,14 @@ class IntbursaryItemInLine(admin.TabularInline):
 # Register your models here.
 
 #class IntbursaryAdmin(admin.ModelAdmin): # the below code willl customise the admin panel (Searching, Filters and display of column headings)
-	
+
 class IntbursaryAdmin(ImportExportModelAdmin):
 	list_display = ('id','surname','first_name','id_number','nationality','gender','race','email','telephone_number','proposed_degree','created_at')
 	list_filter =('proposed_degree','gender','created_at')
 	search_fields =('surname','first_name')
 	list_per_page =15
 	date_hierarchy = 'created_at'
+	
 
 	resource_class = IntbursaryResource #Configure the resource class
 
