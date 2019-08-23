ef9edb450bc4c582066ae5b7ffae0f30b919b793
percevalkm <perceval@sun.ac.za>
2019-08-23 13:09:32 +0200

    tweaked home page

--- archivesuploads/cms_apps.py
+++ archivesuploads/cms_apps.py
@@ -1,14 +1,14 @@
-# from cms.app_base import CMSApp
-# from cms.apphook_pool import apphook_pool
-# from django.utils.translation import ugettext_lazy as _
-#
-#
-# class ArchivesuploadsApphook(CMSApp):
-#     app_name = "archivesuploads"
-#     name = _("Publications Application")
-#
-#     def get_urls(self, page=None, language=None, **kwargs):
-#         return ["archivesuploads.urls"]
-#
-#
-# apphook_pool.register(ArchivesuploadsApphook)  # register the application
+from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
+from django.utils.translation import ugettext_lazy as _
+
+
+class ArchivesuploadsApphook(CMSApp):
+    app_name = "archivesuploads"
+    name = _("Publications Application")
+
+    def get_urls(self, page=None, language=None, **kwargs):
+        return ["archivesuploads.urls"]
+
+
+apphook_pool.register(ArchivesuploadsApphook)  # register the application
